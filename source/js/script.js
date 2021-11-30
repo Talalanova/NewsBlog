e233e1200bd5c109dfb08e61f9804c7664db9478
Talanova <pro_ego@mail.ru>
2021-11-30 17:24:42 +1000

    правки по шрифтам,тест с крупными картинками,вкладки

--- source/js/script.js
+++ source/js/script.js
@@ -13,6 +13,7 @@ openMainNavigation.addEventListener('click', () => {
 const newsPopup = document.querySelector('.popup');
 const openNewsPopup = document.querySelectorAll('.news__expand');
 const closeNewsPopup = document.querySelector('.popup__close');
+const isEscapeKey = (evt) => evt.key === 'Escape';
 
 openNewsPopup.forEach(button => {
   button.addEventListener('click', () => {
@@ -24,11 +25,39 @@ closeNewsPopup.addEventListener('click', () => {
   newsPopup.style.display = "none"
 })
 
+window.addEventListener('keydown', (evt) => {
+  if (isEscapeKey(evt)) {
+    newsPopup.style.display = "none";
+  }
+});
 
-// Прокрутка страницы к началу по кнопке скролла
+// Прокрутка страницы к началу
 
 const topButton = document.querySelector('#scrollToTop')
 
 topButton.addEventListener('click', () => {
   window.scrollTo(0 , 1);
 })
+
+
+// Вкладки по типам авто
+
+const selectTab = element => {
+  const active = document.querySelector('.main-nav__tab--active');
+  const visible = document.querySelector('.news__content--visible');
+  const tabContent = document.getElementById(element.href.split('#')[1]);
+  if (active) {
+    active.classList.remove('main-nav__tab--active');
+  }
+  element.classList.add('main-nav__tab--active');
+  if (visible) {
+    visible.classList.remove('news__content--visible');
+  }
+  tabContent.classList.add('news__content--visible');
+}
+
+document.addEventListener('click', event => {
+  if (event.target.matches('.main-nav__tabs a')) {
+    selectTab(event.target);
+  }
+}, false);
